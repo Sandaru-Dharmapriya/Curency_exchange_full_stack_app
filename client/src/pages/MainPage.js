ef422091df1c7c8e4498a09551bfa9f0f3474939
Sandaru Dharmapriya <148580595+Sandaru-Dharmapriya@users.noreply.github.com>
2024-06-04 18:47:39 +0530

    update

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function MainPage() {
   return (
-    <div>
+    <div className='mt-5 flex items-center justify-center flex-col'>
         <h1 className=' lg:mx-32 text-5xl font-bold text-green-500'>Convert Your Currencies Today</h1>
 
         <p>
@@ -13,10 +13,13 @@ export default function MainPage() {
         </p>
 
         <div>
-            <section>
+            <section className='w-full lg:w-1/2'>
                 <form>
-                    <div>
-                        
+                    <div className='mb-4'>
+                    <label for="date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+                     <input type="date" id="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
+                      focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+                       dark:focus:ring-blue-500 dark:focus:border-blue-500"  required />
                     </div>
                 </form>
             </section>
