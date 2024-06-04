caaff46b5bbc06192109a4cb126c10f45b314c3d
Sandaru Dharmapriya <148580595+Sandaru-Dharmapriya@users.noreply.github.com>
2024-06-04 16:12:46 +0530

    after adding tailwind, create UI

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -3,7 +3,26 @@ import React from 'react'
 export default function MainPage() {
   return (
     <div>
-        <h1>Convert Your Currencies Today</h1>
+        <h1 className=' lg:mx-32 text-5xl font-bold text-green-500'>Convert Your Currencies Today</h1>
+
+        <p>
+        "Step right into "Transform Your Currency Now"!
+        This platform offers seamless currency conversions using the most up-to-date exchange rates.
+        Whether you're plotting a journey, organizing your finances, or just intrigued by the worth 
+        of your money across various currencies, this utility is at your service."
+        </p>
+
+        <div>
+            <section>
+                <form>
+                    <div>
+                        
+                    </div>
+                </form>
+            </section>
+        </div>
     </div>
+    
+    
   )
 }
